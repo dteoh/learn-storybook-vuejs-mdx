069d8995d37f418ce9dc10f71483e68ae761d941
Douglas Teoh <douglas@dteoh.com>
2021-01-12 16:05:07 +0900

    Update unit tests

--- tests/unit/PureTaskList.spec.js
+++ tests/unit/PureTaskList.spec.js
@@ -1,11 +1,11 @@
 import Vue from 'vue';
 import PureTaskList from '../../src/components/PureTaskList.vue';
-import { WithPinnedTasks } from '../../src/components/PureTaskList.stories';
+import { withPinnedTasks } from '../../src/components/PureTaskList.stories.mdx';
 
 it('renders pinned tasks at the start of the list', () => {
   const Constructor = Vue.extend(PureTaskList);
   const vm = new Constructor({
-    propsData: WithPinnedTasks.args,
+    propsData: withPinnedTasks.args,
   }).$mount();
   const firstTaskPinned = vm.$el.querySelector('.list-item:nth-child(1).TASK_PINNED');
 
